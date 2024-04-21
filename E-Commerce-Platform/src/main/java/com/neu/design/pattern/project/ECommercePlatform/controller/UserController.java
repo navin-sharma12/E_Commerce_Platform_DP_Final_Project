bb617e9c3bc960ae3f1a9a8a43a60b064a0f6390
rohitpanicker06 <rohitpanicker06@gmail.com>
2024-04-21 12:52:29 -0400

    Adding versioning

--- E-Commerce-Platform/src/main/java/com/neu/design/pattern/project/ECommercePlatform/controller/UserController.java
+++ E-Commerce-Platform/src/main/java/com/neu/design/pattern/project/ECommercePlatform/controller/UserController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/user")
+@RequestMapping("/v1/user")
 public class UserController {
 
     @Autowired
