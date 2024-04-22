541b7003ba309572911d0743e866ed972d1b134d
jain-apo <114782619+jain-apo@users.noreply.github.com>
2024-04-22 17:59:20 -0400

    Apoorva dev (#57)
    * added login page
    
    * added product flow
    
    * resolved conflict
    
    * added logout
    
    * added remove cart
    
    * resolved merge
    
    * added remove from cart
    
    * added checkout
    
    * added  checkout flow
    
    * added navigation

--- frontend/src/Pages/Checkout/Checkout.js
+++ frontend/src/Pages/Checkout/Checkout.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
 
 const Checkout = () => {
   const [checkoutItems, setCheckoutItems] = useState([]);
@@ -62,6 +63,9 @@ const Checkout = () => {
             ))}
         </div>
       </form>
+      <Link to="/shopping">
+        <button>Continue Shopping</button>
+      </Link>
     </div>
   );
 };
