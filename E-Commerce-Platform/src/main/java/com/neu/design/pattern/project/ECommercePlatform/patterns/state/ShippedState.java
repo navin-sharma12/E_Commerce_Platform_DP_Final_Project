d5d6dbe84ceaab860eb45776941d1af608094541
rohitpanicker06 <rohitpanicker06@gmail.com>
2024-04-22 10:11:54 -0400

    Added loading for transient states

--- E-Commerce-Platform/src/main/java/com/neu/design/pattern/project/ECommercePlatform/patterns/state/ShippedState.java
+++ E-Commerce-Platform/src/main/java/com/neu/design/pattern/project/ECommercePlatform/patterns/state/ShippedState.java
@@ -5,10 +5,12 @@ import com.neu.design.pattern.project.ECommercePlatform.patterns.observer.Order;
 public class ShippedState implements OrderState {
     public void next(Order order) {
         order.setState(new DeliveredState());
+        order.setStateType("delivered");
     }
 
     public void previous(Order order) {
         order.setState(new ProcessingState());
+        order.setStateType("processing");
     }
 
     public void printStatus() {
