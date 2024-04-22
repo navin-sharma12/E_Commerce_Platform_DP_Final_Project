ed4c72e691358100a9b7b29ac5cf7805bcec34b5
rohitpanicker06 <66110853+rohitpanicker06@users.noreply.github.com>
2024-04-22 16:57:18 -0400

    Rohit dev (#55)
    * Adding Spring Boot initilz project with dependencies
    
    * Adding intiial POJO and API Classes
    
    * Moving class to cloneable interface
    
    * Adding JDBC properties, Repositoriers, Login Service, and Strategy + Factory Pattern
    
    * Adding properties
    
    * Adding versioning
    
    * Refactoring package name
    
    * Overriding methods
    
    * Adding join key and foreign indexes between tables
    
    * Adding Builder, decorator, facade, factory, observer, singleton, state pattern
    
    * Adding services and controllers for Order and Product
    
    * Deleting unwanted code
    
    * Added checkout service, discount and invoice functionality
    
    * Making checkout facade as component
    
    * Added imgUrl
    
    * Added loading for transient states
    
    * Adding code for finding all orders
    
    * Bug fixes
    
    * Checkout controller modification
    
    * Changes to cartItemrepo
    
    * Adding removing functionality
    
    * Facade fix
    
    * Making serializer
    
    * Fixing order state
    
    * Fixing service for order
    
    * Adding byId API and multiple ordersAPI

--- E-Commerce-Platform/src/main/java/com/neu/design/pattern/project/ECommercePlatform/patterns/singleton/Cart.java
+++ E-Commerce-Platform/src/main/java/com/neu/design/pattern/project/ECommercePlatform/patterns/singleton/Cart.java
@@ -1,20 +1,16 @@
 package com.neu.design.pattern.project.ECommercePlatform.patterns.singleton;
 
 import com.neu.design.pattern.project.ECommercePlatform.jpa.repository.CartItemsRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Scope;
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
-@Component
 public class Cart {
     private static Cart instance;
 
+    List<CartItem> cartItems = new ArrayList<>();
     private CartItemsRepository cartItemsRepository;
 
     private Cart() {
@@ -27,20 +23,29 @@ public class Cart {
         return instance;
     }
 
-    // Dependency setter
     public void setCartItemsRepository(CartItemsRepository repository) {
         this.cartItemsRepository = repository;
     }
 
+    @Transactional
     public void addItem(CartItem item) {
-        cartItemsRepository.save(item);
+        cartItems.add(item);
+//        cartItemsRepository.save(item);
     }
 
+    @Transactional
     public void removeItem(CartItem item) {
-        cartItemsRepository.delete(item);
+        cartItems.remove(item);
+//        cartItemsRepository.delete(item);
     }
 
     public List<CartItem> getItems() {
-      return  cartItemsRepository.findAll();
+      return cartItems;
     }
+
+    public void removeAllItems()
+    {
+        cartItems.clear();
+    }
+
 }
