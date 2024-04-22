8e7bf0a2ff4ab6b114116277589ce5b28b005e3b
Moinuddin Khaja <khaja.m@northeastern.edu>
2024-04-22 14:58:04 -0400

    Checkout Page

--- frontend/src/Pages/Checkout/Checkout.js
+++ frontend/src/Pages/Checkout/Checkout.js
@@ -1,12 +1,31 @@
 import React, { useState } from "react";
 
 const Checkout = () => {
+    const [checkoutItems, setCheckoutItems] = useState([]);
+
+    const fetchCheckout = async () => {
+        try {
+          const response = await fetch("http://localhost:8080/v1/checkout");
+          const data = await response.json();
+          console.log("checkout -  ", data);
+          setCheckoutItems(data);
+        } catch (error) {
+          console.error("Error displaying checkout:", error);
+        }
+      };
+
+    useEffect(() => {
+        fetchCheckout();
+      }, []);
 
   return (
-    <div>
-      <h2>Checkout</h2>
+    <div className="checkout-page">
+      <h1>Checkout Page</h1>
+       <div>
+           <p>{checkoutItems}</p>
+        </div>
     </div>
   );
-};
+}
 
 export default Checkout;
