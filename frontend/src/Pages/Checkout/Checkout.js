70faa75c55d898bccff2adf2551fcf9437ccb875
jain-apo <114782619+jain-apo@users.noreply.github.com>
2024-04-22 17:39:26 -0400

    Apoorva dev (#56)
    * added login page
    
    * added product flow
    
    * resolved conflict
    
    * added logout
    
    * added remove cart
    
    * resolved merge
    
    * added remove from cart
    
    * added checkout
    
    * added  checkout flow

--- frontend/src/Pages/Checkout/Checkout.js
+++ frontend/src/Pages/Checkout/Checkout.js
@@ -1,32 +1,69 @@
 import React, { useState, useEffect } from "react";
 
 const Checkout = () => {
-    const [checkoutItems, setCheckoutItems] = useState([]);
+  const [checkoutItems, setCheckoutItems] = useState([]);
+  const {
+    "Invoice Date": invoiceDate,
+    "Order ID": orderId,
+    Items: items,
+  } = checkoutItems;
+  const fetchCheckout = async () => {
+    console.log("Checkout:Fetch");
+    try {
+      const response = await fetch("http://localhost:8080/v1/checkout");
+      const data = await response.json();
+      console.log("checkout data -  ", data);
+      setCheckoutItems(data);
+    } catch (error) {
+      console.error("Error displaying checkout:", error);
+    }
+  };
+  console.log("checkout -  ", checkoutItems);
 
-    const fetchCheckout = async () => {
-        try {
-          const response = await fetch("http://localhost:8080/v1/checkout");
-          const data = await response.json();
-          console.log("checkout -  ", data);
-          setCheckoutItems(data);
-        } catch (error) {
-          console.error("Error displaying checkout:", error);
-        }
-      };
-
-    useEffect(() => {
-        fetchCheckout();
-      }, []);
+  useEffect(() => {
+    console.log("Checkout:UseEffect");
+    fetchCheckout();
+  }, []);
 
   return (
-    <div className="checkout-page">
+    <div
+      style={{
+        backgroundColor: "orange",
+        marging: "20px",
+        marginTop: "80px",
+        marginLeft: "25%",
+        padding: "20px",
+        height: "400px",
+        width: "500px",
+      }}
+    >
       <h1>Checkout Page</h1>
-       <div>
-           <p>{checkoutItems}</p>
+      <form>
+        <div>
+          <label>Invoice Date:</label>
+          <input type="text" value={invoiceDate} readOnly />
         </div>
+        <div>
+          <label>Order ID:</label>
+          <input type="text" value={orderId} readOnly />
+        </div>
+        <div>
+          <label>Items:</label>
+          {items &&
+            items.map((item, index) => (
+              <div key={index}>
+                <label>Product Name:</label>
+                <input type="text" value={item["Product Name"]} readOnly />
+                <label>Price:</label>
+                <input type="text" value={item.Price} readOnly />
+                <label>Quantity:</label>
+                <input type="text" value={item.Quantity} readOnly />
+              </div>
+            ))}
+        </div>
+      </form>
     </div>
   );
 };
-
 
 export default Checkout;
