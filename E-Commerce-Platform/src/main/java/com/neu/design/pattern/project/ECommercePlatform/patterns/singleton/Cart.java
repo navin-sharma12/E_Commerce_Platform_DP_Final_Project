23b71da18072e1c42e7d0b955cb0e5a1f6eaa23d
rohitpanicker06 <66110853+rohitpanicker06@users.noreply.github.com>
2024-04-22 14:11:32 -0400

    Rohit dev (#39)
    * Adding Spring Boot initilz project with dependencies
    
    * Adding intiial POJO and API Classes
    
    * Moving class to cloneable interface
    
    * Adding JDBC properties, Repositoriers, Login Service, and Strategy + Factory Pattern
    
    * Adding properties
    
    * Adding versioning
    
    * Refactoring package name
    
    * Overriding methods
    
    * Adding join key and foreign indexes between tables
    
    * Adding Builder, decorator, facade, factory, observer, singleton, state pattern
    
    * Adding services and controllers for Order and Product
    
    * Deleting unwanted code
    
    * Added checkout service, discount and invoice functionality
    
    * Making checkout facade as component
    
    * Added imgUrl
    
    * Added loading for transient states
    
    * Adding code for finding all orders
    
    * Bug fixes
    
    * Checkout controller modification
    
    * Changes to cartItemrepo

--- E-Commerce-Platform/src/main/java/com/neu/design/pattern/project/ECommercePlatform/patterns/singleton/Cart.java
+++ E-Commerce-Platform/src/main/java/com/neu/design/pattern/project/ECommercePlatform/patterns/singleton/Cart.java
@@ -1,11 +1,17 @@
 package com.neu.design.pattern.project.ECommercePlatform.patterns.singleton;
 
+import com.neu.design.pattern.project.ECommercePlatform.jpa.repository.CartItemsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class Cart {
     private static Cart instance;
     private List<CartItem> items;
+
+    @Autowired
+    CartItemsRepository cartItemsRepository;
 
     private Cart() {
         items = new ArrayList<>();
@@ -20,10 +26,12 @@ public class Cart {
 
     public void addItem(CartItem item) {
         items.add(item);
+        cartItemsRepository.save(item);
     }
 
     public void removeItem(CartItem item) {
         items.remove(item);
+        cartItemsRepository.delete(item);
     }
 
     public List<CartItem> getItems() {
