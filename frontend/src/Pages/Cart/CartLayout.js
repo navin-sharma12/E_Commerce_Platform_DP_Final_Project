b94336ebcd50e8e192a9e788431eb6a7345d6a08
Apoorva Jain <jain.apo@northeastern.edu>
2024-04-22 19:29:41 -0400

    added cart UI

--- frontend/src/Pages/Cart/CartLayout.js
+++ frontend/src/Pages/Cart/CartLayout.js
@@ -6,27 +6,33 @@ import "./cart.css";
 function CartLayout({ items, fetchCartItems }) {
   const total = items.reduce(
     // (acc, item) => acc + item.price * item.quantity,
-    (acc, item) => acc + item.product.price*item.product.stockQuantity,
+    (acc, item) => acc + item.product.price * item.product.stockQuantity,
     0
   );
 
   return (
-    <>
-      <h3>Shopping Cart</h3>
+    <div className="cart-layout">
+      {/* <h3>Shopping Cart</h3> */}
       <div className="cart">
-        {items.map((item, index) => (
-          <CartItem key={index} item={item} fetchCartItems={fetchCartItems} />
-        ))}
+        {items.length > 0 &&
+          items.map((item, index) => (
+            <CartItem key={index} item={item} fetchCartItems={fetchCartItems} />
+          ))}
       </div>
-      <h3>Total: ${total.toFixed(2)}</h3>
+      <h3 style={{ height: "50px", width: "300px", alignContent: "center" }}>
+        Total: ${total.toFixed(2)}
+      </h3>
       <Link to="/shopping">
-        <button>Continue Shopping</button>
+        <button style={{ height: "50px", width: "300px" }}>
+          Continue Shopping
+        </button>
       </Link>
       <Link to="/Checkout">
-        <button>Proceed to Checkout</button>
+        <button style={{ height: "50px", width: "300px" }}>
+          Proceed to Checkout
+        </button>
       </Link>
-    </>
-
+    </div>
   );
 }
 
