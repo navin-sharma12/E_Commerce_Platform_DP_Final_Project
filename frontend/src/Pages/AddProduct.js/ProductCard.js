458afef0769fc23981f9980323966c751f2fe259
Apoorva Jain <jain.apo@northeastern.edu>
2024-04-22 14:13:44 -0400

    added remove cart

--- frontend/src/Pages/AddProduct.js/ProductCard.js
+++ frontend/src/Pages/AddProduct.js/ProductCard.js
@@ -1,22 +1,48 @@
 import React from "react";
 import axios from "axios";
 import "./product.css";
+import displayToast from "../../utils/displayToast";
 
 const ProductCard = ({ product }) => {
   const handleAddToCart = async () => {
-      try {
-        await axios.post("http://localhost:8080/v1/cart/add", product);
-      } catch (error) {
-        console.error("Error adding item to cart:", error);
-      }
-    };
+    fetch("http://localhost:8080/v1/cart/add", {
+      method: "POST",
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        quantity: 1,
+        product: {
+          id: product.id,
+          name: product.name,
+          price: product.price,
+          stockQuantity: product.stockQuantity,
+          imgUrl: product.imgUrl,
+        },
+      }),
+    })
+      .then((response) => {
+        if (response.status == 200) {
+          displayToast({
+            type: "success",
+            msg: "Product added successfully to  the cart!",
+          });
+        }
+      })
+      .catch((err) => {
+        displayToast({ type: "error", msg: "Product addition unsuccessful." });
+        console.log(err);
+      });
+  };
+
   return (
     <div>
       <div className="card">
-        <img src={product.image} alt={product.name} />
+        <img src={product.imgUrl} alt={product.name} />
         <div className="card-body">
           <h2>{product.name}</h2>
-          <p>{product.description}</p>
+          <p>{product.stockQuantity}</p>
           <p>Price: ${product.price}</p>
           <button onClick={handleAddToCart}>Add to Cart</button>
         </div>
