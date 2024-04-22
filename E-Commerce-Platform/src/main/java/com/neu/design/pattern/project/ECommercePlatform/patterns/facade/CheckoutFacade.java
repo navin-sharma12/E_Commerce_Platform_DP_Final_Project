1a9e620dd6bab879837abe9158f64c39fb87667e
rohitpanicker06 <rohitpanicker06@gmail.com>
2024-04-22 08:25:41 -0400

    Added checkout service, discount and invoice functionality

--- E-Commerce-Platform/src/main/java/com/neu/design/pattern/project/ECommercePlatform/patterns/facade/CheckoutFacade.java
+++ E-Commerce-Platform/src/main/java/com/neu/design/pattern/project/ECommercePlatform/patterns/facade/CheckoutFacade.java
@@ -1,18 +1,27 @@
 package com.neu.design.pattern.project.ECommercePlatform.patterns.facade;
 
+import com.neu.design.pattern.project.ECommercePlatform.models.InvoiceFactory;
 import com.neu.design.pattern.project.ECommercePlatform.patterns.builder.OrderBuilder;
 import com.neu.design.pattern.project.ECommercePlatform.patterns.observer.Order;
 import com.neu.design.pattern.project.ECommercePlatform.patterns.singleton.Cart;
 import com.neu.design.pattern.project.ECommercePlatform.patterns.singleton.CartItem;
+import com.neu.design.pattern.project.ECommercePlatform.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.ArrayList;
 
 public class CheckoutFacade {
+
+    @Autowired
+    private OrderService orderService;
+
+    @Autowired
+    private InvoiceFactory invoiceFactory;
+
     public void completeCheckout(Cart cart) {
-        OrderBuilder builder = new OrderBuilder();
-        for (CartItem item : cart.getItems()) {
-            builder.addItem(item);
-        }
-        Order order = builder.build();
+        Order order = orderService.createOrder(new ArrayList<>(cart.getItems()));
         order.placeOrder();
+        String invoice = invoiceFactory.generateInvoice(order);
         order.printStatus();
     }
 }
